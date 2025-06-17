aadb9c6d84747c6de77bd15a49b48a452c3d60d9
prave01 <praveenthanikachalam408@gmail.com>
2025-06-17 22:45:31 +0530

    Ollama integrated

---
+++ apps/api/src/Controllers/chat.ts
@@ -0,0 +1,26 @@
+import OpenAI from "openai";
+
+const client = new OpenAI({
+	baseURL: "http://127.0.0.1:11434/v1",
+	apiKey: "open-ai", // This can be any string with Ollama
+});
+
+async function run(): any {
+ try {
+  const response = await client.chat.completions.create({
+   model: "gemma3:4b",
+   messages: [
+    {
+     role: "user",
+     content: "Give me top 10 cooking recipes that I must try",
+    },
+   ],
+  });
+
+  console.log(response.choices[0].message.content);
+ } catch (error) {
+  console.error("Error:", error);
+ }
+}
+
+run();
