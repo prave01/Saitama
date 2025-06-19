6ea80a12cf98d2e22303d117442ac5d6f89ba973
prave01 <praveenthanikachalam408@gmail.com>
2025-06-19 12:02:35 +0530

    Creting llm client class

--- apps/api/src/Controllers/chat.ts
+++ apps/api/src/Controllers/chat.ts
@@ -1,26 +1,19 @@
+import { StringLiteralUnion } from "hono/utils/types";
 import OpenAI from "openai";
 
-const client = new OpenAI({
-	baseURL: "http://127.0.0.1:11434/v1",
-	apiKey: "open-ai", // This can be any string with Ollama
-});
+class LLM_Client {
+ private client_url: string;
+ private client_name: string;
+ private client?: OpenAI;
 
-async function run(){
- try {
-  const response = await client.chat.completions.create({
-   model: "gemma3:4b",
-   messages: [
-    {
-     role: "user",
-     content: "Give me top 10 cooking recipes that I must try",
-    },
-   ],
-  });
-
-  console.log(response.choices[0].message.content);
- } catch (error) {
-  console.error("Error:", error);
+ constructor(clientUrl: string, clientName: string) {
+  this.client_url = clientUrl;
+  this.client_name = clientName;
+  this.client = undefined;
  }
-}
 
-run();
+ // Create llm client instance with the OpenAi Api
+ createClient() { }
+
+ runLLM() { }
+}
